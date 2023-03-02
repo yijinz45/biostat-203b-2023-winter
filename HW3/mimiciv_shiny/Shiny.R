319b3904b444aa84e6b2b0cc3a9ba14c5485b35f
Aokiao <Aoki@zhaoyijindeMBP3.lan>
2023-03-01 18:35:08 -0800

    final version of shiny app

--- HW3/mimiciv_shiny/Shiny.R
+++ HW3/mimiciv_shiny/Shiny.R
@@ -10,15 +10,16 @@ library(lubridate)
 icu_cohort <- readRDS("icu_cohort.rds")
 
 ## demographics
-demographvar <- c("age_at_admission","gender", "ethnicity", "language", 
+demographvar <- c("age_at_admission", "gender", "ethnicity", "language", 
              "insurance", "marital_status", "Bicarbonate", 
              "Chloride", "Creatinine", "Glucose", "Potassium", "Sodium",
-             "Hematocrit", "White_Blood_Cells","Heart_Rate", 
+             "Hematocrit", "White_Blood_Cells", "Heart_Rate", 
               "Non_Invasive_Blood_Pressure_systolic",
               "Non_Invasive_Blood_Pressure_mean", "Respiratory_Rate",
               "Temperature_Fahrenheit")
 #boxplot variable
-boxplotvar <- c("age_at_admission","Bicarbonate", "Chloride", "Creatinine", "Glucose", "Potassium",
+boxplotvar <- c("age_at_admission","Bicarbonate", "Chloride", "Creatinine", 
+                "Glucose", "Potassium",
                 "Sodium", "Hematocrit", "White_Blood_Cells", "Heart_Rate", 
                 "Non_Invasive_Blood_Pressure_systolic", 
                 "Non_Invasive_Blood_Pressure_mean", 
@@ -53,6 +54,7 @@ ui <- fluidPage(
       # Output: 
       helpText("Plot of selected variable:"),
       plotOutput(outputId = "demoboxplot"),
+      helpText("Summary of selected variable (Counts for categorical): "),
       verbatimTextOutput("summary")
     )
   )
@@ -60,7 +62,7 @@ ui <- fluidPage(
 #create the server
 server <- function(input, output) {
   output$demoboxplot <- renderPlot({
-#separate the categoricol and the quantitative    
+#separate the categoricol and the quantitative for boxplots   
     if(input$demo %in% boxplotvar){
       
       icu_cohort %>%
@@ -71,21 +73,20 @@ server <- function(input, output) {
         xlab(input$demo) + labs(title = paste0("Boxplot of ", input$demo))
       
       } else {
-        icu_cohort %>%
-          ggplot(., aes_string(x = input$demo)) +
-          geom_bar(aes_string(x = input$demo, fill = icu_cohort$day30mort)) +
-          labs(title = paste0("Bar chart of ", input$demo))
+      icu_cohort %>%
+        ggplot(., aes_string(x = input$demo)) +
+        geom_bar(aes_string(x = input$demo, fill = icu_cohort$day30mort)) +
+        labs(title = paste0("Bar chart of ", input$demo))
       }
   })
   #create the table chart in the ui
   output$summary <- renderPrint({
     if (input$demo %in% boxplotvar) {
-    summary(icu_cohort[input$demo])
-      } else
-      {
-    table(icu_cohort[input$demo])   
-      }
-  })
+      summary(icu_cohort[input$demo])
+      } else {
+          table(icu_cohort[input$demo])   
+   }
+ })
 }
-#run the dataset
+#create the app
 shinyApp(ui = ui, server = server)
